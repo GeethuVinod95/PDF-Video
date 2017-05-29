a3a88e8c29186d42c24ec5bbcdec154f074f0a74
CORP\Geethu.Adhikarveedu <geethu.adhikarveedu@neudesic.com>
2017-05-29 12:58:53 +0530

    First commit

---
+++ PdfVideo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestPdfFileWriter;
+
+namespace PdfVideo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new OtherExample();
+            p.Test(false, @"C:\Test\sample6y.pdf");
+        }
+    }
+}
